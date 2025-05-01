822f811a6cedc67ee4a086ef7659bfb1391f68d4
m355145 <104318038+m355145@users.noreply.github.com>
2025-05-01 11:53:35 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import docs from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://seusite.netlify.app', // Altere para o seu domínio se quiser
+  integrations: [
+    docs({
+      sidebar: [
+        { label: 'Início', link: '/' },
+        { label: 'Compartilhe', link: '/docs/index' },
+      ],
+      title: 'Teste Social',
+      description: 'Descubra quem realmente presta atenção em você',
+      logo: {
+        light: '/logo-light.svg',
+        dark: '/logo-dark.svg'
+      }
+    })
+  ],
 });
